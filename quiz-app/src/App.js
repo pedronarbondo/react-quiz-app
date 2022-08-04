8d2f98a1057b419154e8265c96c4d20d53fa2a4a
pedronarbondo <pedro.narbondo@hotmail.com>
2022-08-04 11:01:49 -0300

    Create almost functioning app

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,45 +1,63 @@
 import React from "react"
 import Quiz from "./components/Quiz"
+import {nanoid} from "nanoid"
+import Shuffle from "./components/Shuffle"
 
 function App() {
   
   React.useEffect(() => {
   fetch("https://opentdb.com/api.php?amount=5")
     .then(res => res.json())
-    .then(data => setData(data.results))
-    console.log(data)
+    .then(datas => setData(datas.results))
   },[])
 
-  const [data, setData] = React.useState([])
-  const [quizzes, setQuizzes] = React.useState({})
   const [playing, setPlaying] = React.useState(false)
+  const [data, setData] = React.useState([])
+  const [myQuiz, setMyQuiz] = React.useState([])
 
-  function startGame() {
-    setPlaying(prevPlaying => !prevPlaying)
 
-    data.map(dataPoint => {
-      setQuizzes({
-        question: dataPoint.question,
-        correctAnswer: dataPoint.correct_answer,
-        incorrectAnswers: dataPoint.incorrect_answers,
-        difficulty: dataPoint.difficulty
+
+ function setupQuizData(){
+    return (
+      data.map(datapoint => {
+        setMyQuiz(prevQuestions => {
+          return [...prevQuestions, {
+            question: datapoint.question,
+            correctAnswer: datapoint.correct_answer,
+            incorrectAnswers: datapoint.incorrect_answers,
+            difficulty: datapoint.difficulty,
+            id: nanoid()
+          }]
+        })
       })
-    })
+    )
   }
 
 
-
-
+  function setupQuiz() { 
+      Shuffle(myQuiz)
+      return myQuiz.map(quiz => {
+        return (
+            <Quiz 
+            question={quiz.question}
+            correctAnswer={quiz.correctAnswer}
+            incorrectAnswers={quiz.incorrectAnswers}
+            difficulty={quiz.difficulty}
+            key={quiz.id}/>
+        )
+    })
+  }
+  
+  function startGame() {
+    setPlaying(prevPlaying => !prevPlaying)
+    setupQuizData()
+  }
   
   return (
     playing ?
     <div className="main">
       <div className="quiz--holder">
-        <Quiz />
-        <Quiz />
-        <Quiz />
-        <Quiz />
-        <Quiz />
+       {setupQuiz()}
       </div>
     </div>
     : 
--- quiz-app/src/components/Quiz.js
+++ quiz-app/src/components/Quiz.js
@@ -1,17 +1,70 @@
 import React from "react"
+import Shuffle from "./Shuffle"
+import {nanoid} from "nanoid"
 
 export default function Quiz(props){
+
+    const [answerData, setAnswerData] = React.useState([
+        {
+            answer: props.correctAnswer,
+            correct: true,
+            selected: false,
+            id: nanoid()
+        },
+        {
+            answer: props.incorrectAnswers[0],
+            correct: false,
+            selected: false,
+            id: nanoid()
+        },
+        {
+            answer: props.incorrectAnswers[1],
+            correct: false,
+            selected: false,
+            id: nanoid()
+        },
+        {
+            answer: props.incorrectAnswers[2],
+            correct: false,
+            selected: false,
+            id: nanoid()
+        }
+    ])
+
+    function handleClick(id) {
+        setAnswerData(prevData => {
+            return prevData.map(answer => {
+                return answer.id === id ? {...answer, selected: !answer.selected} : {...answer, selected: false}
+            })
+        })
+    }
+
+
+    function makeButtons() {
+        return answerData.map(answer => {
+        
+            return (
+                <button 
+                className={`answer-button ${answer.selected ? "answer--selected" : ""}`}
+                correct={answer.correct ? 1 : 0}
+                selected={answer.selected ? 1 : 0}
+                key={nanoid()}
+                id={answer.id}
+                onClick={() => handleClick(answer.id)}
+                > {answer.answer}
+                </button>
+            )
+        })
+    }
+
 
     return (
         <div className="quiz">
             <h4 className="quiz--title">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, sed?
+                {props.question} 
             </h4>
             <div className="quiz--buttons">
-                <button className="answer-button">Option 1</button>
-                <button className="answer-button">Option 2</button>
-                <button className="answer-button">Option 3</button>
-                <button className="answer-button">Option 4</button>
+                {makeButtons()}
             </div>
             <hr className="line-break"></hr>
         </div>
