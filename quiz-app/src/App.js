0daa88ea93f0f90acf524339f4aa32a32a86d6a9
pedronarbondo <pedro.narbondo@hotmail.com>
2022-08-04 12:10:54 -0300

    Restart project

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,49 +1,16 @@
 import React from "react"
 import {nanoid} from "nanoid"
-import Main from "./components/Main"
 
 function App() {
-  
-  React.useEffect(() => {
-  fetch("https://opentdb.com/api.php?amount=5")
-    .then(res => res.json())
-    .then(datas => setData(datas.results))
-  },[])
-
   const [playing, setPlaying] = React.useState(false)
-  const [data, setData] = React.useState([])
-  const [myQuiz, setMyQuiz] = React.useState([])
 
-
-
- function setupQuizData(){
-    return (
-      data.map(datapoint => {
-        setMyQuiz(prevQuestions => {
-          return [...prevQuestions, {
-            question: datapoint.question,
-            correctAnswer: datapoint.correct_answer,
-            incorrectAnswers: datapoint.incorrect_answers,
-            difficulty: datapoint.difficulty,
-            id: nanoid()
-          }]
-        })
-      })
-    )
-  }
-
-  
   function startGame() {
     setPlaying(prevPlaying => !prevPlaying)
-    setupQuizData()
   }
   
   return (
     playing ?
     <div className="main">
-      <div className="quiz--holder">
-       <Main quiz={myQuiz} />
-      </div>
     </div>
     : 
 
